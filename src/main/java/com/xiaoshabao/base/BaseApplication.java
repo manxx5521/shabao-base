b3b26c082c993b5ec3c943be51237fbc389c4130
xv <xv@xv-PC>
2018-04-19 00:30:12 +0800

    1

--- src/main/java/com/xiaoshabao/base/BaseApplication.java
+++ src/main/java/com/xiaoshabao/base/BaseApplication.java
@@ -1,10 +1,8 @@
 package com.xiaoshabao.base;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
 @ComponentScan(basePackages="com.xiaoshabao.base.controller")
 @ComponentScan(basePackages="com.xiaoshabao.base.dao")
 @ComponentScan(basePackages="com.xiaoshabao.base.service")
