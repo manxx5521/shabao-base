343e2c83dc5fdf924b363134b8313459b2e9fa61
xv <xv@xv-PC>
2018-05-19 16:46:15 +0800

    可选文件存储方式构建

--- src/main/java/com/xiaoshabao/base/component/oss/OSSFactory.java
+++ src/main/java/com/xiaoshabao/base/component/oss/OSSFactory.java
@@ -3,7 +3,6 @@ package com.xiaoshabao.base.component.oss;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 import com.xiaoshabao.base.component.SpringContextHolder;
@@ -14,34 +13,44 @@ import com.xiaoshabao.base.component.sysConfig.SysConfig;
  * 文件上传Factory
  */
 @Component
-@Lazy
 public class OSSFactory {
 	@Autowired
 	private SysConfig sysConfig;
-    
-    /**
-     * 创建存储对象功能接口
-     * @return
-     */
-    public StorageAble build() {
-    	Integer type=sysConfig.getInteger(StorageConstant.typeId);
-    	Map<String,StorageAble> ables=SpringContextHolder.getApplicationContext().getBeansOfType(StorageAble.class);
-    	
-    	for (Map.Entry<String,StorageAble> entry : ables.entrySet()) {
-            String name=entry.getKey();
-            if((OSSConstant.ablePrefix+type).equals(name)) {
-            	StorageAble able= entry.getValue();
-            	able.initConfig();
-            	return able;
-    		}
-        }
-    	
-        /*if (type.equals(OSSConstant.Type.LOCAL)) {
-            return SpringContextHolder.getBean(LocalStorageService.class);
-        } else if (type.equals(OSSConstant.Type.LOCAL)) {
-            return SpringContextHolder.getBean(AliyunStorageService.class);
-        } */
-        return null;
-    }
+
+	/**
+	 * 创建存储对象功能接口
+	 */
+	public StorageAble build() {
+		Integer type = sysConfig.getInteger(StorageConstant.typeId);
+		return build(type);
+	}
+
+	/**
+	 * 创建存储对象功能接口
+	 * 
+	 * @param type
+	 *            存储类型 {@link OSSConstant.Type}
+	 */
+	public StorageAble build(Integer type) {
+		Map<String, StorageAble> ables = SpringContextHolder
+				.getApplicationContext().getBeansOfType(StorageAble.class);
+
+		for (Map.Entry<String, StorageAble> entry : ables.entrySet()) {
+			String name = entry.getKey();
+			if ((OSSConstant.ablePrefix + type).equals(name)) {
+				StorageAble able = entry.getValue();
+				able.initConfig();
+				return able;
+			}
+		}
+
+		/*
+		 * if (type.equals(OSSConstant.Type.LOCAL)) { return
+		 * SpringContextHolder.getBean(LocalStorageService.class); } else if
+		 * (type.equals(OSSConstant.Type.LOCAL)) { return
+		 * SpringContextHolder.getBean(AliyunStorageService.class); }
+		 */
+		return null;
+	}
 
 }
